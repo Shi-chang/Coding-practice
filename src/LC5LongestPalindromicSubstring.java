577d963f8db7d19520be7040c61e46001f23ef99
Shi-chang <shichangye@outlook.com>
2023-01-20 11:10:57 -0800

    Add LC5LongestPalindromicSubstring

---
+++ src/LC5LongestPalindromicSubstring.java
@@ -0,0 +1,35 @@
+public class LC5LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+        String result = s.substring(0, 1);
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            int[] oddPalindrome = palindromeIndices(s, i, i);
+            int[] evenPalindrome = palindromeIndices(s, i, i + 1);
+            if (oddPalindrome[1] - oddPalindrome[0] + 1 > result.length()) {
+                result = s.substring(oddPalindrome[0], oddPalindrome[1] + 1);
+            }
+            if (evenPalindrome[1] - evenPalindrome[0] + 1 > result.length()) {
+                result = s.substring(evenPalindrome[0], evenPalindrome[1] + 1);
+            }
+        }
+
+        return result;
+    }
+
+    private int[] palindromeIndices(String s, int left, int right) {
+        // a b c
+        // l r   -> {l, l}
+        if (s.charAt(left) != s.charAt(right)) {
+            return new int[]{left, left};
+        }
+
+        // a b b c
+        //   l r   -> {l, r}
+        while (left >= 0 && right <= s.length() - 1 && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        return new int[]{left + 1, right - 1};
+    }
+}
