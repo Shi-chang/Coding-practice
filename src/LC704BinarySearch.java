a1ff9619ceb783b213efbf2572ece3ae8ca3676d
Shi-chang <shichangye@outlook.com>
2023-01-22 09:29:26 -0800

    Add LC704BinarySearch

---
+++ src/LC704BinarySearch.java
@@ -0,0 +1,26 @@
+/**
+ * Given an array of integers nums which is sorted in ascending order, and an integer target, write
+ * a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+ */
+public class LC704BinarySearch {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] > target) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        return -1;
+    }
+}
