45bb2e3e26daac2bc50f4663305b4f946c2849c2
Shi-chang <shichangye@outlook.com>
2023-01-30 11:15:33 -0800

    Add AlgoExpertSmallestDifference

---
+++ src/AlgoExpertSmallestDifference.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class AlgoExpertSmallestDifference {
+
+    public static int[] smallestDifference(int[] arrayOne, int[] arrayTwo) {
+        // Write your code here.
+        Arrays.sort(arrayOne);
+        Arrays.sort(arrayTwo);
+
+        int[] result = new int[]{arrayOne[0], arrayTwo[0]};
+
+        int p1 = 0;
+        int p2 = 0;
+
+        while (p1 < arrayOne.length && p2 < arrayTwo.length) {
+            if (arrayOne[p1] == arrayTwo[p2]) {
+                return new int[]{arrayOne[p1], arrayTwo[p2]};
+            }
+            if (arrayOne[p1] < arrayTwo[p2]) {
+                if (arrayTwo[p2] - arrayOne[p1] < Math.abs(result[1] - result[0])) {
+                    result[0] = arrayOne[p1];
+                    result[1] = arrayTwo[p2];
+                }
+                p1++;
+            } else {
+                if (arrayOne[p1] - arrayTwo[p2] < Math.abs(result[1] - result[0])) {
+                    result[0] = arrayOne[p1];
+                    result[1] = arrayTwo[p2];
+                }
+                p2++;
+            }
+        }
+
+        return result;
+    }
+}
