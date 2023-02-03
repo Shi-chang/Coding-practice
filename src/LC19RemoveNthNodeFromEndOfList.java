b9cfb1704b0796d2c328beb53f247c1a8c67fba3
Shi-chang <shichangye@outlook.com>
2023-02-02 22:38:16 -0800

    Complete LC19RemoveNthNodeFromEndOfList

---
+++ src/LC19RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,45 @@
+//Definition for singly-linked list.
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+// example N = 2
+// d 1 2 3 4 5
+//         x
+// f
+// s
+public class LC19RemoveNthNodeFromEndOfList {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1, head);
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
