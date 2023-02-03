59340c0d10bd3e473d0077dfe1ca3bfbca62c79d
Shi-chang <shichangye@outlook.com>
2023-02-03 08:21:24 -0800

    Complete LC206ReverseLinkedList

---
+++ src/LC206ReverseLinkedList.java
@@ -0,0 +1,19 @@
+/**
+ * LC206. Reverse Linked List
+ */
+public class LC206ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode cur = head;
+
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+
+        return prev;
+    }
+}
