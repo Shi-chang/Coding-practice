bd6b18048a53cf2e8d5348cc64a8b4d3f2b730e9
Shi-chang <shichangye@outlook.com>
2023-02-11 11:31:47 -0800

    Complete LC136SingleNumber

---
+++ src/LC136SingleNumber.java
@@ -0,0 +1,11 @@
+public class LC136SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res = res ^ num;
+        }
+
+        return res;
+    }
+}
